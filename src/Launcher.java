2f5f6f96dbc348cd7a597bebd7fbc3e9d1b9416d
MathisCABOSTE <mathis.caboste@epita.fr>
2022-09-20 15:14:40 +0200

    Partie 4 - une première commande

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,10 +1,24 @@
 public class Launcher {
+    public static int fibo(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fibo(n - 1) + fibo(n - 2);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Bienvenue !");           
+        System.out.println("Bienvenue !");
         var scanner = new java.util.Scanner(System.in);
         var input = scanner.nextLine();
-        while (!input.equals("quit")) {
-            System.out.println("Unkown command");
+        while (!(input.equals("quit"))) {
+            if (input.equals("fibo")) {
+                System.out.println("Ecrivez le nombre dont vous souhaitez voir la suite de FIbonacci: ");
+                int n = scanner.nextInt();
+                scanner.nextLine();
+                System.out.println(fibo(n));
+            } else {
+                System.out.println("Unkown command");
+            }
             input = scanner.nextLine();
         }
         scanner.close();
